d05f6a533e958ee81117c1e5bcf002b32bd0064b
JK <kntrvch@gmail.com>
2019-01-21 01:20:55 +0100

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    mode: 'development',
+    entry: __dirname + '/src/index.js',
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/dist/',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: 'babel-loader'
+        }]
+    }
+};
